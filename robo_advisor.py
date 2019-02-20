c44e8f11b33141845c8a7f35e7c93699427bc493
Colin Mills <cgm71@georgetown.edu>
2019-02-20 17:05:11 -0500

    create a data structure to export to csv

--- robo_advisor.py
+++ robo_advisor.py
@@ -3,6 +3,7 @@ import json
 import os
 import requests
 from dotenv import load_dotenv
+from pandas import DataFrame
 
 #load_dotenv() #> loads contents of the .env file into the script's environment
 #
@@ -31,7 +32,7 @@ if stockTicker.isalpha() and len(stockTicker) <= 4:
         parsed_response = json.loads(response.text)
 
 
-        print(parsed_response["Time Series (Daily)"]["2019-02-19"]["4. close"])
+        #print(parsed_response["Time Series (Daily)"]["2019-02-19"]["4. close"])
         # > '1627.5800'
 
         tsd = parsed_response["Time Series (Daily)"] #> 'dict'
@@ -39,11 +40,13 @@ if stockTicker.isalpha() and len(stockTicker) <= 4:
         # What keys or attributes does this dictionary have?
         # ... see: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/datatypes/dictionaries.md
         day_keys = tsd.keys() #> 'dict_keys' of all the day values
+
         #
         # convert weird dict_keys datatype to a list so we can work with it!
         days = list(day_keys) #> 'list' of all the day values
-        print(days[0]) # 'str' of the latest day!
-        latest_day = days[0] #> '2019-02-19'
+        #print(days)
+        #print(days[0]) # 'str' of the latest day!
+        #latest_day = days[0] #> '2019-02-19'
         #
         #print(parsed_response["Time Series (Daily)"][latest_day]["4. close"])
         # > '1627.5800'
@@ -56,6 +59,38 @@ if stockTicker.isalpha() and len(stockTicker) <= 4:
 
         print("-----------------------")
         print("CRUNCHING THE DATA...")
+
+        timeStamps = []
+
+        opens = []
+        highs = []
+        lows = []
+        closes = []
+        volumes = []
+
+        for date in days:
+            timeStamps.append(str(date))
+            opens.append(tsd[date]["1. open"])
+            highs.append(tsd[date]["2. high"])
+            lows.append(tsd[date]["3. low"])
+            closes.append(tsd[date]["4. close"])
+            volumes.append(tsd[date]["5. volume"])
+           
+
+        Stocks = {
+                'timestamp': timeStamps,
+                'open': opens,
+                'high': highs,
+                'low': lows, 
+                'close': closes,
+                'volume': volumes
+                }
+
+        df = DataFrame(Stocks, columns= ['timestamps', 'open','high','low','close','volume'])
+
+        #export_csv = df.to_csv (r"robo_advisor\data\prices_{}.csv".format(stockTicker), index = None, header=True) #Don't forget to add '.csv' at the end of the path
+
+        print (df)
 
         print("-----------------------")
         print("LATEST CLOSING PRICE: $1,259.19")
